c29641bdef9b8111cf1a5803a0f0149d79a0f074
Demet Gökçe <demet.gokce@bilgeadamakademi.com>
2022-05-31 13:55:23 +0300

    wwwroot dist dosyası eklendi

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -11,15 +11,27 @@ namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Bilge Kolej";
+        private readonly ILogger<HomeController> _logger;
 
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
             return View();
         }
-        public ActionResult GirisYap()
+
+        public IActionResult Privacy()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
