3d4c9e4d7c2709b8ab25ebaceb4826f57354ca1f
Demet Gökçe <demet.gokce@bilgeadamakademi.com>
2022-06-02 01:13:01 +0300

    bilgeDbContext kullanici eklendi

--- Bilge.Domain/BilgeDbContext.cs
+++ Bilge.Domain/BilgeDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Okul.Domain;
 using System;
 using System.Collections.Generic;
 
@@ -22,8 +23,8 @@ namespace Bilge.Domain
         public DbSet<DonemDers> DonemDers { get; set; }
         public DbSet<OgrenciDonemDers> OgrenciDonemDers { get; set; }
         public DbSet<Yoklama> Yoklamalar { get; set; }
-        
 
+        public DbSet<Kullanici> Kullanicilar { get; set; }
         public BilgeDbContext()
         {
 
@@ -36,13 +37,25 @@ namespace Bilge.Domain
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            //optionsBuilder.UseSqlServer(@"Server=.;Database=OkulDb5142;User Id=sa;password=123");
-            optionsBuilder.UseNpgsql(@"Server=127.0.0.1;Port=5432;Database=BilgeDb;User Id=postgres;Password=123;");
+            optionsBuilder.UseSqlServer(@"Server=.;Database=OkulDb;User Id=sa;password=123");
+            //optionsBuilder.UseNpgsql(@"Server=127.0.0.1;Port=5432;Database=BilgeDb;User Id=postgres;Password=123;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+           
 
+
+            #region Sinif
+            modelBuilder.Entity<Siniflar>().Property(e => e.SinifAdi).HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<Siniflar>().HasData(
+                new Siniflar { SinifAdi = "9A", Kapasite = 25,Kod="100", Id = 1 },
+                new Siniflar { SinifAdi = "10A", Kapasite = 25, Kod = "101", Id = 2 },
+                new Siniflar { SinifAdi = "11A", Kapasite = 25, Kod = "102", Id = 3 },
+                new Siniflar { SinifAdi = "12A", Kapasite = 25, Kod = "103", Id = 4 }
+                );
+
+            #endregion
 
         }
     }
